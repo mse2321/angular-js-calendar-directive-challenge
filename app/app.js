84cfcc13f0511fb31278c08ec54c1ef8161b91aa
mse2321 <mse233@gmail.com>
2016-03-16 18:41:25 -0400

    finished styling and logic

--- app/app.js
+++ app/app.js
@@ -1,37 +1,56 @@
 var demo  = angular.module('calendarDemoApp', []);
 
 demo.controller('ctrl', function($scope){
-	//$scope.month = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
-	$scope.month = [];
 	$scope.now = new Date();
-	$scope.currentYear = $scope.now.getFullYear();
+	$scope.month = $scope.now.getMonth() + 1;
+	$scope.selectedYear = $scope.now.getFullYear();
+	$scope.day = $scope.now.getDate();
 	$scope.years = [];
 
-	var yearRangeStart = $scope.currentYear - 10;
-	var yearRangeEnd = $scope.currentYear + 10;
+	var yearRangeStart = $scope.selectedYear - 10;
+	var yearRangeEnd = $scope.selectedYear + 10;
 
 	for(var i = yearRangeStart ; i < yearRangeEnd; i++) {
       $scope.years.push(i);
     }
 
 	$scope.monthList = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-	$scope.monthDayAmount = [31,29,31,30,31,30,31,31,30,31,30,31];
-	for(var i = 0 ; i < $scope.monthDayAmount.length; i++) {
-      $scope.month.push($scope.monthDayAmount[i]);
-      console.log($scope.month);
-    }
+	$scope.selectedMonth = $scope.monthList[$scope.now.getMonth()];
 
-	$scope.currentMonth = $scope.monthList[$scope.now.getMonth()];
+    $scope.chosenDate = {
+    	month: $scope.selectedMonth,
+    	year: $scope.selectedYear
+    };
+
+	$scope.currentMonthCheck = function(currentMonth){
+		return (currentMonth == $scope.monthList.indexOf($scope.chosenDate.month));
+	};
+
+	$scope.$watchCollection('chosenDate', function(chosenDate){
+		$scope.updatedDate = chosenDate.month + ', ' + chosenDate.year;
+		$scope.range = CalendarRange.getMonthlyRange(new Date($scope.updatedDate));
+		$scope.initialDay = $scope.range.first;
+		$scope.firstDay = $scope.range.start;
+		$scope.finalDay = $scope.range.last;
+		$scope.lastDay = $scope.range.end;
+		$scope.days = $scope.range.days;
+	});
 
 })
-// your controller and directive code go here
+
 demo.directive('calendar', function() {
 	return {
-		template: '<div class="day_wrap" ng-repeat="day in month track by $index">{{ day }}</div>'
+		templateUrl: 'calendar-template.html',
+		restrict: 'E',
+		scope: true,
+		transclude: true
 	}
 })
 demo.directive('calendarNav', function() {
 	return {
-		template: '<select ng-model="currentMonth" ng-options="item as item for item in monthList"></select><select ng-model="currentYear" ng-options="year as year for year in years"></select>'
+		templateUrl: 'calendar-nav-template.html',
+		restrict: 'E',
+		scope: true,
+		transclude: true
 	}
 });
